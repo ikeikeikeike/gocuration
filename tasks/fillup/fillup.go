d92fb1c9250531ea13480d414989e96484c10af2
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-03-12 11:43:40 +0900

    Change own host(domain) referer.

--- tasks/fillup/fillup.go
+++ tasks/fillup/fillup.go
@@ -15,13 +15,13 @@ import (
 
 func availableFilechecker() *image.Info {
 	info := image.NewInfo()
-	info.Header("Referer", "http://localhost:8080")
 	info.Header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 	info.Header("Accept-Encoding", "gzip, deflate, sdch")
 	info.Header("Accept-Language", "ja,en-US;q=0.8,en;q=0.6,zh;q=0.4,zh-TW;q=0.2,ko;q=0.2,es;q=0.2,ru;q=0.2")
 	info.Header("Cache-Control", "no-cache")
 	info.Header("Connection", "keep-alive")
 	info.Header("Pragma", "no-cache")
+	info.Header("Referer", fmt.Sprintf("http://%s", beego.AppConfig.String("domain"))) // Own host(domain) referer.
 	info.Header("User-Agent", beego.AppConfig.String("UserAgent"))
 	return info
 }
