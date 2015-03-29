3badfe0a6f4a79166ddfc6c1cf701fda412f3148
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-03-29 22:50:44 +0900

    fillup kana in chara and anime table

--- tasks/fillup/anime.go
+++ tasks/fillup/anime.go
@@ -127,3 +127,50 @@ func AnimeImageByGoogleimages() (err error) {
 	err = FillupFromGoogleimages(animes, "エロアニメ")
 	return
 }
+
+// func AnimeKanaByMecab(option string) (err error) {
+// option = fmt.Sprintf("-d %s", option)
+
+// var animes []*models.Anime
+// models.Animes().RelatedSel().Limit(1000000).All(&animes)
+
+// for _, m := range animes {
+
+// // "-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd"
+// result, err := mecab.Parse(m.Name, option)
+// if err != nil {
+// beego.Error(err)
+// continue
+// }
+
+// var name, kana, hira, roma []string
+// for _, res := range result {
+// name = append(name, res.Surface)
+// roma = append(roma, res.Romaji)
+// kana = append(kana, res.Read)
+// hira = append(hira, res.Hiragana)
+// }
+
+// // namae := strings.Join(name, "")
+
+// if m.Kana == "" {
+// m.Kana = strings.Join(kana, "")
+// }
+// if m.Romaji == "" {
+// m.Romaji = strings.Join(roma, " ")
+// }
+// if m.Gyou == "" {
+// hiragana := strings.Join(hira, "")
+// if hiragana != "" {
+// result, _ = mecab.Parse(string([]rune(hiragana)[0]), option)
+// for _, r := range result {
+// m.Gyou = r.Kunrei
+// }
+// }
+// }
+
+// m.Update("Kana", "Romaji", "Gyou")
+// }
+
+// return
+// }
