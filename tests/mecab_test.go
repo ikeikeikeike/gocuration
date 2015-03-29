defa31157f32cf7e1bcc4c390df86b7886944a4b
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-03-29 22:50:47 +0900

    fillup kana in chara and anime table

--- tests/mecab_test.go
+++ tests/mecab_test.go
@@ -4,67 +4,54 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/astaxie/beego"
 	"github.com/ikeikeikeike/gopkg/mecab"
-	"github.com/k0kubun/pp"
 
 	_ "bitbucket.org/ikeikeikeike/antenna/conf/inits"
 	"bitbucket.org/ikeikeikeike/antenna/models"
 	_ "bitbucket.org/ikeikeikeike/antenna/routers"
 )
 
-func TestSentenceParse(t *testing.T) {
-	input := "すもももももももものうち"
+func TestFillupCharacterByMecab(t *testing.T) {
+	option := "-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd"
 
-	result, err := mecab.Parse(input, "")
-	if err != nil {
-		t.Error(err)
-	}
+	var list []*models.Character
+	models.Characters().RelatedSel().Limit(1000000).All(&list)
 
-	for _, res := range result {
-		pp.Println(res)
-	}
-}
+	for _, m := range list {
 
-// func TestDivaParse(t *testing.T) {
-// input := "すもももももももものうち"
-
-// result, err := mecab.Parse(input)
-// if err != nil {
-// t.Error(err)
-// }
-
-// for _, res := range result {
-// pp.Println(res)
-// }
-// }
-
-func TestAnimeParse(t *testing.T) {
-
-	var animes []*models.Anime
-	models.Animes().All(&animes)
-
-	for _, m := range animes {
-		pp.Println("==================")
-
-		result, err := mecab.Parse(m.Name, "-d /usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd")
+		result, err := mecab.Parse(m.Name, option)
 		if err != nil {
-			t.Error(err)
+			beego.Error(err)
 			continue
 		}
 
-		var n, r, hira, roma []string
+		var name, kana, hira, roma []string
 		for _, res := range result {
-			pp.Println(res)
-			n = append(n, res.Surface)
-			r = append(r, res.Read)
-			hira = append(hira, res.Hiragana)
+			name = append(name, res.Surface)
 			roma = append(roma, res.Romaji)
+			kana = append(kana, res.Read)
+			hira = append(hira, res.Hiragana)
 		}
 
-		pp.Println(strings.Join(n, ""))
-		pp.Println(strings.Join(r, ""))
-		pp.Println(strings.Join(hira, ""))
-		pp.Println(strings.Join(roma, " "))
+		// namae := strings.Join(name, "")
 
+		if m.Kana == "" {
+			m.Kana = strings.Join(kana, "")
+		}
+		if m.Romaji == "" {
+			m.Romaji = strings.Join(roma, " ")
+		}
+		if m.Gyou == "" {
+			hiragana := strings.Join(hira, "")
+			if hiragana != "" {
+				result, _ = mecab.Parse(string([]rune(hiragana)[0]), option)
+				for _, r := range result {
+					m.Gyou = r.Kunrei
+				}
+			}
+		}
+
+		m.Update("Kana", "Romaji", "Gyou")
 	}
 }
