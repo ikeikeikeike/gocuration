c45d58bbd2c1439fde7e02615f77d5179457b7f4
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-04-11 18:52:58 +0900

    Addd relation model loader method to entry and picture model

--- ormapper/entry.go
+++ ormapper/entry.go
@@ -29,8 +29,39 @@ type Entry struct {
 	Picture *Picture
 	Summary *Summary
 
-	Tags   []Tag   `gorm:"many2many:entry_tag;"`
-	Images []Image `gorm:"many2many:entry_image;"`
+	Tags   []*Tag   `gorm:"many2many:entry_tag;"`
+	Images []*Image `gorm:"many2many:entry_image;"`
 
-	Scores []Score
+	Scores []*Score
+}
+
+func (m *Entry) RelLoader() {
+	if m.Summary != nil {
+		DB.Model(&m).Preload("Entry").Preload("Scores").
+			Related(&m.Summary)
+	}
+	if m.Blog != nil {
+		DB.Model(&m).Preload("User").Preload("Icon").Preload("Scores").Preload("Entries").
+			Related(&m.Blog)
+	}
+	if m.Video != nil {
+		DB.Model(&m).Preload("Entry").Preload("Site").
+			//Preload("Divas").
+			Related(&m.Video)
+	}
+	if m.Picture != nil {
+		DB.Model(&m).Preload("Entry").Preload("Anime").Preload("Images").
+			//Preload("Characters").
+			Related(&m.Picture)
+	}
+
+	DB.Model(&m).Preload("Entry").Preload("Blog").Preload("Summary").
+		Related(&m.Scores)
+
+	DB.Model(&m).Preload("Image").Preload("Entries").Association("Tags").
+		Find(&m.Tags)
+
+	DB.Model(&m).Preload("Picture").Association("Images").
+		Find(&m.Images)
+
 }
--- ormapper/picture.go
+++ ormapper/picture.go
@@ -17,6 +17,23 @@ type Picture struct {
 	Anime   *Anime
 	AnimeId sql.NullInt64
 
-	Images     []Image
-	Characters []Character `gorm:"many2many:picture_character;"`
+	Images     []*Image
+	Characters []*Character `gorm:"many2many:picture_character;"`
+}
+
+func (m *Picture) RelLoader() {
+	if m.Entry != nil {
+		DB.Model(&m).
+			Preload("Picture").Preload("Video").Preload("Summary").Preload("Blog").
+			Preload("Scores").
+			// Preload("Tags").Preload("Images")
+			Related(&m.Entry)
+	}
+	if m.Anime != nil {
+		DB.Model(&m).Preload("Icon").Preload("Characters").Preload("Pictures").
+			Related(&m.Anime)
+	}
+
+	DB.Model(&m).Preload("Picture").
+		Related(&m.Images)
 }
