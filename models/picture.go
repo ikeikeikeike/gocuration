57b7ce5e5e7513bb561287789872861a9e1696e4
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-06-01 18:13:23 +0900

    model schema

--- models/picture.go
+++ models/picture.go
@@ -10,8 +10,9 @@ import (
 )
 
 type Picture struct {
-	Id       int64 `orm:"auto"`
-	PageView int64 `orm:"default(0);index"`
+	Id         int64 `orm:"auto"`
+	PageView   int64 `orm:"default(0);index"`
+	ImageCount int   `orm:"default(0);index"`
 
 	Created time.Time `orm:"auto_now_add;type(datetime)"`
 	Updated time.Time `orm:"auto_now;type(datetime)"`
