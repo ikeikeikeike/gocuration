670f8a36f445aa6723cc45d9b0f927826dccd415
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-06-05 02:13:21 +0900

    ranking model for each sites

--- models/ranking.go
+++ models/ranking.go
@@ -5,14 +5,12 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-
-	"github.com/ikeikeikeike/gopkg/convert"
 )
 
-type Ranking struct {
+type EntryRanking struct {
 	Id int64 `orm:"auto"`
 
-	BeginName string    `orm:"size(255);index"` // dayly, weekly, monthly, yearly
+	BeginName string    `orm:"size(255);index" valid:"Required;Match(/^(dayly|weekly|monthly|yearly)$/)"` // dayly, weekly, monthly, yearly
 	BeginTime time.Time `orm:"type(datetime);index"`
 
 	Rank     int64 `orm:"default(0);index"` // rank order number
@@ -24,58 +22,78 @@ type Ranking struct {
 	Entry *Entry `orm:"rel(fk);index"`
 }
 
-func (m *Ranking) LoadRelated() *Ranking {
+func (m *EntryRanking) LoadRelated() *EntryRanking {
 	o := orm.NewOrm()
 	_, _ = o.LoadRelated(m, "Entry", 2, DefaultPerEntities)
 	return m
 }
 
-func (m *Ranking) RelLoader() {
-	m.LoadRelated()
-}
-
-func (m *Ranking) IdStr() string {
-	return convert.ToStr(m.Id)
-}
-
-func (m *Ranking) Insert() error {
-	if _, err := orm.NewOrm().Insert(m); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (m *Ranking) Read(fields ...string) error {
-	if err := orm.NewOrm().Read(m, fields...); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (m *Ranking) ReadOrCreate(field string, fields ...string) (bool, int64, error) {
+func (m *EntryRanking) ReadOrCreate(field string, fields ...string) (bool, int64, error) {
 	return orm.NewOrm().ReadOrCreate(m, field, fields...)
 }
 
-func (m *Ranking) Update(fields ...string) error {
-	if _, err := orm.NewOrm().Update(m, fields...); err != nil {
-		return err
-	}
-	return nil
+type VideoRanking struct {
+	Id int64 `orm:"auto"`
+
+	BeginName string    `orm:"size(255);index" valid:"Required;Match(/^(dayly|weekly|monthly|yearly)$/)"` // dayly, weekly, monthly, yearly
+	BeginTime time.Time `orm:"type(datetime);index"`
+
+	Rank     int64 `orm:"default(0);index"` // rank order number
+	PageView int64 `orm:"default(0);index"` // 1,2,3,4
+
+	Created time.Time `orm:"auto_now_add;type(datetime)"`
+	Updated time.Time `orm:"auto_now;type(datetime)"`
+
+	Video *Video `orm:"rel(fk);index"`
 }
 
-func (m *Ranking) Delete() error {
-	if _, err := orm.NewOrm().Delete(m); err != nil {
-		return err
-	}
-	return nil
+func (m *VideoRanking) LoadRelated() *VideoRanking {
+	o := orm.NewOrm()
+	_, _ = o.LoadRelated(m, "Video", 2, DefaultPerEntities)
+	return m
+}
+func (m *VideoRanking) ReadOrCreate(field string, fields ...string) (bool, int64, error) {
+	return orm.NewOrm().ReadOrCreate(m, field, fields...)
 }
 
-func Rankings() orm.QuerySeter {
-	return orm.NewOrm().QueryTable("ranking").OrderBy("-Id")
+type PictureRanking struct {
+	Id int64 `orm:"auto"`
+
+	BeginName string    `orm:"size(255);index" valid:"Required;Match(/^(dayly|weekly|monthly|yearly)$/)"` // dayly, weekly, monthly, yearly
+	BeginTime time.Time `orm:"type(datetime);index"`
+
+	Rank     int64 `orm:"default(0);index"` // rank order number
+	PageView int64 `orm:"default(0);index"` // 1,2,3,4
+
+	Created time.Time `orm:"auto_now_add;type(datetime)"`
+	Updated time.Time `orm:"auto_now;type(datetime)"`
+
+	Picture *Picture `orm:"rel(fk);index"`
+}
+
+func (m *PictureRanking) LoadRelated() *PictureRanking {
+	o := orm.NewOrm()
+	_, _ = o.LoadRelated(m, "Picture", 2, DefaultPerEntities)
+	return m
+}
+func (m *PictureRanking) ReadOrCreate(field string, fields ...string) (bool, int64, error) {
+	return orm.NewOrm().ReadOrCreate(m, field, fields...)
+}
+
+func EntryRankings() orm.QuerySeter {
+	return orm.NewOrm().QueryTable("entry_ranking").OrderBy("-Id")
+}
+
+func VideoRankings() orm.QuerySeter {
+	return orm.NewOrm().QueryTable("video_ranking").OrderBy("-Id")
+}
+
+func PictureRankings() orm.QuerySeter {
+	return orm.NewOrm().QueryTable("picture_ranking").OrderBy("-Id")
 }
 
 func init() {
-	orm.RegisterModelWithPrefix(
-		beego.AppConfig.String("dbprefix"),
-		new(Ranking))
+	orm.RegisterModelWithPrefix(beego.AppConfig.String("dbprefix"), new(EntryRanking))
+	orm.RegisterModelWithPrefix(beego.AppConfig.String("dbprefix"), new(VideoRanking))
+	orm.RegisterModelWithPrefix(beego.AppConfig.String("dbprefix"), new(PictureRanking))
 }
