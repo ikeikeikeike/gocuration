ec795fba1828e0e3ca712d778fdec93ed26cf18e
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-06-09 23:10:09 +0900

    rankings code in book site

--- controllers/book/rankings.go
+++ controllers/book/rankings.go
@@ -28,3 +28,60 @@ func (c *RankingsController) Dayly() {
 
 	c.Data["Rankings"] = rankings
 }
+
+func (c *RankingsController) Weekly() {
+	c.TplNames = "book/rankings/weekly.tpl"
+
+	db := ormapper.PictureRankings().
+		Where("picture_ranking.rank > ?", 0).
+		Where("picture_ranking.begin_name = ?", "weekly").
+		Where("picture_ranking.begin_time = ?", c.GetParamatedNow().BeginningOfWeek()).
+		Limit(100)
+
+	var rankings []*ormapper.PictureRanking
+	db.Order("picture_ranking.rank ASC").Find(&rankings)
+
+	for _, s := range rankings {
+		s.RankingsLoader()
+	}
+
+	c.Data["Rankings"] = rankings
+}
+
+func (c *RankingsController) Monthly() {
+	c.TplNames = "book/rankings/monthly.tpl"
+
+	db := ormapper.PictureRankings().
+		Where("picture_ranking.rank > ?", 0).
+		Where("picture_ranking.begin_name = ?", "monthly").
+		Where("picture_ranking.begin_time = ?", c.GetParamatedNow().BeginningOfMonth()).
+		Limit(100)
+
+	var rankings []*ormapper.PictureRanking
+	db.Order("picture_ranking.rank ASC").Find(&rankings)
+
+	for _, s := range rankings {
+		s.RankingsLoader()
+	}
+
+	c.Data["Rankings"] = rankings
+}
+
+func (c *RankingsController) Yearly() {
+	c.TplNames = "book/rankings/yearly.tpl"
+
+	db := ormapper.PictureRankings().
+		Where("picture_ranking.rank > ?", 0).
+		Where("picture_ranking.begin_name = ?", "yearly").
+		Where("picture_ranking.begin_time = ?", c.GetParamatedNow().BeginningOfYear()).
+		Limit(100)
+
+	var rankings []*ormapper.PictureRanking
+	db.Order("picture_ranking.rank ASC").Find(&rankings)
+
+	for _, s := range rankings {
+		s.RankingsLoader()
+	}
+
+	c.Data["Rankings"] = rankings
+}
--- routers/book/router.go
+++ routers/book/router.go
@@ -19,9 +19,9 @@ func init() {
 	beego.Router(`/book/viewer:id([0-9]+)/:title(.*)?`, &book.EntriesController{}, "get:Viewer")
 
 	beego.Router("/ranking/dayly", &book.RankingsController{}, "get:Dayly")
-	// beego.Router("/ranking/weekly", &book.RankingsController{}, "get:Weekly")
-	// beego.Router("/ranking/monthly", &book.RankingsController{}, "get:Monthly")
-	// beego.Router("/ranking/yearly", &book.RankingsController{}, "get:Yearly")
+	beego.Router("/ranking/weekly", &book.RankingsController{}, "get:Weekly")
+	beego.Router("/ranking/monthly", &book.RankingsController{}, "get:Monthly")
+	beego.Router("/ranking/yearly", &book.RankingsController{}, "get:Yearly")
 
 	beego.Router("/search.html", &book.EntriesController{}, "get:Search")
 
